d552d5f241100f4728007c9bbfa548d87f125a4b
jesse <jesseai@astri.org>
2015-11-03 19:02:37 +0800

    sqlx prepared statement cache

--- db/sql.go
+++ db/sql.go
@@ -1,15 +1,16 @@
-package utils
+package db
 
 import (
 	"bytes"
 	"reflect"
+
+	"github.com/aiyi/go/utils"
 )
 
-func SqlUpdateSetArgs(s *bytes.Buffer, para interface{}) int {
-	var args []interface{}
+func SqlUpdateSetArgs(s *bytes.Buffer, para interface{}, args []interface{}) int {
 	x := 0
-
 	v := reflect.Indirect(reflect.ValueOf(para))
+
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
 		if field.Kind() == reflect.Ptr || field.Kind() == reflect.Slice {
@@ -20,7 +21,7 @@ func SqlUpdateSetArgs(s *bytes.Buffer, para interface{}) int {
 			if x > 0 {
 				s.WriteString(", ")
 			}
-			s.WriteString(ToFieldName(v.Type().Field(i).Name))
+			s.WriteString(utils.ToFieldName(v.Type().Field(i).Name))
 			s.WriteString("=?")
 			args = append(args, field.Elem())
 			x++
