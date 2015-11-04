36f04f9333d3506927e02515a49884b8bf1811d0
jesse <jesseai@astri.org>
2015-11-04 13:01:26 +0800

    bug fix

--- db/sql.go
+++ db/sql.go
@@ -7,23 +7,27 @@ import (
 	"github.com/aiyi/go/utils"
 )
 
-func SqlUpdateSetArgs(s *bytes.Buffer, para interface{}, args []interface{}) int {
+func SqlUpdateSetArgs(s *bytes.Buffer, para interface{}, args *[]interface{}) int {
 	x := 0
 	v := reflect.Indirect(reflect.ValueOf(para))
 
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
-		if field.Kind() == reflect.Ptr || field.Kind() == reflect.Slice {
+		if field.Kind() != reflect.Ptr && field.Kind() != reflect.Slice {
 			continue
 		}
 
-		if field.Elem().IsValid() {
+		if field.IsNil() == false {
 			if x > 0 {
 				s.WriteString(", ")
 			}
 			s.WriteString(utils.ToFieldName(v.Type().Field(i).Name))
 			s.WriteString("=?")
-			args = append(args, field.Elem())
+			if field.Kind() == reflect.Ptr {
+				*args = append(*args, field.Elem().Interface())
+			} else {
+				*args = append(*args, field.Slice(0, field.Len()).Interface())
+			}
 			x++
 		}
 	}
